45ebee46b8d2c1fdef210de07791760c00a5b511
mohammed <mohammed@ad.attlsoft.com>
2026-08-06 09:28:06 +0300

    Fix import paths for Vercel Linux

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -25,7 +25,7 @@ export default function HomePage() {
   
   const isRightToLeft = i18n.language === 'ar';
   
-  const username = localStorage.getItem('username') || 'محمد عبدالحكيم قائد محمد';
+  const username = localStorage.getItem('username') || 'محمد عبدالحكيم قائد ';
   const branchName = localStorage.getItem('branchName') ||'فرع الرياض - الرئيسي - شارع العليا الفرعي';
 
   useEffect(() => {
